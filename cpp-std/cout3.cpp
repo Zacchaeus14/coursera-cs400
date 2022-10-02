06a65bd6ee9e27fdfa43611d91794b3fa3465034
Zacchaeus <yw3642@nyu.edu>
2022-10-02 03:54:28 -0400

    std cout3

---
+++ cpp-std/cout3.cpp
@@ -0,0 +1,15 @@
+/**
+ * C++ program for a simple "Hello, world!" with `using`.
+ * 
+ * @author
+ *   Wade Fagen-Ulmschneider <waf@illinois.edu>
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello, world!" << endl;
+  return 0;
+}
